3545a2a7efef21c749e81ee7c5879ecaa8ae0a8b
jagrutipatil <jagrutipatil32@gmail.com>
2016-05-19 16:08:51 -0700

    mongodb

--- routes/cmpe_mongodb.js
+++ routes/cmpe_mongodb.js
@@ -4,13 +4,20 @@ var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://ec2-52-36-58-189.us-west-2.compute.amazonaws.com:27017,ec2-52-40-92-106.us-west-2.compute.amazonaws.com:27017,ec2-52-40-53-118.us-west-2.compute.amazonaws.com:27017/test?w=0&readPreference=secondary";
 
 function getData(req, res) {
+
 	MongoClient.connect(url, function(err, db) {
-			var cursor = db.collection('mycol').find();
-			cursor.each(function(err, doc) {	
-				  if (doc != null) {
-					  console.log("key is "+doc.key);
-				  }
-			});			
+		var cursor =db.collection('mycol').find();
+		var doc_total = {};
+		var index = 0;
+		cursor.each(function(err, doc) {
+	      console.log("Doc is "+doc)
+	      if (doc != null) {
+	         doc_total[index++] = doc;
+	      } else {
+	    	  db.close();
+	    	  res.end(JSON.stringify((doc_total),null,4));
+	      }
+	   });
 	});
 }
 
